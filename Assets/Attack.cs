237cc7d25ca9f6f13fe9b129ea93e9b48e7e95fa
maxichai <maxichai@gmail.com>
2019-08-03 16:05:25 -0700

    Changed the slash spawn

--- Assets/Attack.cs
+++ Assets/Attack.cs
@@ -6,6 +6,8 @@ public class Attack : MonoBehaviour
 {
     Vector3 mouse;
     Vector3 mouse2;
+    public GameObject slashPrefab;
+    GameObject a;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +20,15 @@ public class Attack : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mouse.x, mouse.y, -9);
-
-        } else if (Input.GetMouseButton(0))
+            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);               
+        } else if (Input.GetMouseButtonUp(0))
         {
             mouse2 = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             mouse2.Normalize();
+            Vector3 spawn = new Vector3(mouse.x, mouse.y, -9);
             float rot_z = Mathf.Atan2(mouse2.y, mouse2.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);           
+            a = Instantiate(slashPrefab, spawn, Quaternion.identity);
+            a.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
         }
         
     }
--- Assets/slash.cs
+++ Assets/slash.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class slash : MonoBehaviour
 {
     private Vector2 screenBounds;
-    public GameObject player;
     float speed;
     private Collision col;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0.01f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * speed;
-        Destroy(gameObject, 15f);
+        Destroy(gameObject, 2f);
     }
 }
