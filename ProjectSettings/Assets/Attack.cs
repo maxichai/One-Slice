440d2a6d1b8f4bb65babc84c5c4af45f88f5a44f
maxichai <maxichai@gmail.com>
2019-08-02 22:15:21 -0700

    Uploaded all Unity files

---
+++ ProjectSettings/Assets/Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    Vector3 mouse;
+    public GameObject slashPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position);
+        mouse = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(mouse.x + " " + mouse.y + " " + mouse.z);
+            GameObject a = Instantiate(slashPrefab) as GameObject;
+            a.transform.position = transform.position;
+
+        }
+        
+    }
+    Vector3 mousePosition()
+    {
+        return mouse;
+    }
+}
---
+++ ProjectSettings/Assets/slash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slash : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    public gameObject player;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.4f;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.GetComponent.mousePosition(), speed);
+    }
+}
