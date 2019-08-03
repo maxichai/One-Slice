1c616ec8fe9b1d3c39cbb56644a933a4a32d8b6e
Lorem <22178748+Yaoeh@users.noreply.github.com>
2019-08-03 15:29:45 -0400

    completed scene transitions

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public float fadeSpeed = 0.5f;
+    public void transition(string sceneName) {
+        Initiate.Fade(sceneName, Color.black, fadeSpeed);
+    }
+}
