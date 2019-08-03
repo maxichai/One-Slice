b9eb74ae84ba86a24c8490851109ed022cd8aa9f
Lorem <22178748+Yaoeh@users.noreply.github.com>
2019-08-03 18:23:55 -0400

    added swing animation on mouse up

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class PlayerCharacter : Character
 {
+    public Animator anim;
     public override void OnHealthChange() {
         base.OnHealthChange();
 
         if (Health <= 0) {
             Initiate.Fade("00 Main Menu", Color.black, 0.5f);
+        }
+    }
+
+    public void Attack() {
+        anim.SetTrigger("Attack");
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            Attack();
         }
     }
 }
