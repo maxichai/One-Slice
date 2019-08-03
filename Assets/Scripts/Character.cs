f059fbac221188c32c55e8c9835ef6412c89d9ed
Lorem <22178748+Yaoeh@users.noreply.github.com>
2019-08-03 18:18:29 -0400

    set up game master, character

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private int health = 1;
+    private int maxHealth = 1;
+    public int Health {
+        get {
+            return health;
+        }
+        set {
+            health = Mathf.Clamp(value, 0, maxHealth);
+            OnHealthChange();
+        }
+    }
+
+    public int MaxHealth {
+        get {
+            return maxHealth;
+        }
+        set {
+            maxHealth = Mathf.Clamp(value, 0, int.MaxValue);
+            health = Mathf.Clamp(health, 0, maxHealth);
+            OnHealthChange();
+        }
+    }
+
+    public void Damage(int damage) {
+        Health -= Mathf.Clamp(damage, 0, int.MaxValue); //should never end up adding health 
+    }
+
+    public virtual void OnHealthChange() {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : Character
+{
+    public override void OnHealthChange() {
+        base.OnHealthChange();
+
+        if (Health <= 0) {
+            Initiate.Fade("00 Main Menu", Color.black, 0.5f);
+        }
+    }
+}
